14cb1146367a012ee7bdfe6e4019141b044974c0
RoshanatNiser <roshan.yadav@niser.ac.in>
2025-08-21 18:01:59 +0530

    Update Assgn_3.py

--- Assgn_3.py
+++ Assgn_3.py
@@ -5,15 +5,43 @@
 
 from Func_lib_for_assgn_3 import *
 
+# Question 1: LU Decomposition using Doolittle method
+print("Question 1: LU Decomposition using Doolittle method")
 
-# Question 1:
-A= read_matrix('A.txt')
-L,U=LU(A)
+# Read matrix A from file
+A = read_matrix("A.txt")
 
-P=matmul(L,U)
 
-print('A=',A)
-print('L=',L)
-print('U=',U)
-print('L.U=',P)
+# Get LU decomposition
+result = LU_decomposition(A)
 
+print("\nLU Decomposition Result:",result) 
+
+'''Output of question 1: [[1.0, 2.0, 3.0], [3.0, 2.0, 5.0], [2.0, 1.0, 2.0]]'''
+
+
+# Question 2: Solve linear equations using LU decomposition
+print("\nQuestion 2: Solving Linear Equations using LU decomposition")
+
+# Read matrix C and d from file
+C = read_matrix("C.txt")
+d = read_vector("d.txt")  
+
+
+# Solve 
+solution = solve_linear_equations(C, d)
+
+print("\nSolution vector x:")
+for i, val in enumerate(solution):
+    print(f"a{i+1} = {round(val, 6)}")
+
+print("\n")
+
+''' Output: Solution vector x:
+a1 = -3.158521
+a2 = 2.592302
+a3 = 6.991849
+a4 = -3.461831
+a5 = 2.722799
+a6 = 1.368153
+'''
