1a857bc7a355a853cb961331c6eb0bbb0dbd1ee7
RoshanatNiser <roshan.yadav@niser.ac.in>
2025-08-21 18:01:25 +0530

    Update Func_lib_for_assgn_3.py

--- Func_lib_for_assgn_3.py
+++ Func_lib_for_assgn_3.py
@@ -1,11 +1,9 @@
-#This script contains functions for LU decomposition and solving linear equations using the LU decomposition method.
-# # Name: Roshan Yadav
+# Assignment 3: LU Decomposition Functions
+# Name: Roshan Yadav
 # Roll No: 2311144
-
-from typing import List, Union
+# Method Used: Doolittle
 
 def read_matrix(filename):
-
     with open( filename , 'r' ) as f :
         matrix =[]
         for line in f :
@@ -14,85 +12,85 @@ def read_matrix(filename):
             matrix.append(row)
     return matrix
 
-def matmul(M: List[List[float]], N: List[List[float]]) -> Union[str, List[List[float]]]:
+def read_vector(filename):
     """
-    Perform matrix multiplication of two matrices M and N.
-    
-    Args:
-        M (List[List[float]]): First matrix (m*k)
-        N (List[List[float]]): Second matrix (k*n)
-        
-    Returns:
-        Union[str, List[List[float]]]: Result matrix or error message
-        
-    Note:
-        For matrix multiplication MN to be valid:
-        - Number of columns in M must equal number of rows in N
-        - Result will be an (m*n) matrix
+    Read a vector from a file (one number per line)
     """
-    if not M or not N or not M[0] or not N[0]:
-        return "Error: Empty matrices provided"
-    
-    rows_M, cols_M = len(M), len(M[0])
-    rows_N, cols_N = len(N), len(N[0])
-    
-    # Check if multiplication is possible
-    if cols_M != rows_N:
-        return f"Error: Cannot multiply {rows_M}*{cols_M} matrix with {rows_N}*{cols_N} matrix"
-    
-    # Initialize result matrix with zeros
-    result = [[0.0 for _ in range(cols_N)] for _ in range(rows_M)]
-    
-    # Perform matrix multiplication
-    for i in range(rows_M):
-        for j in range(cols_N):
-            for k in range(cols_M):
-                result[i][j] += M[i][k] * N[k][j]
-    
-    return result
+    with open(filename, 'r') as f:
+        vector = []
+        for line in f:
+            vector.append(float(line.strip()))
+    return vector
 
-
-def LU(A):
+def LU_decomposition(A):
     """
-    Perform LU decomposition of matrix A
-    using Doolittle.
+    This function recturns LU decomposition of matrix A using Doolittle method.
 
     """
     n = len(A)
-    L=[]
-    U=[]
-    for i in range(n):
-        K=[]
-        for j in range(n):
-            K.append(0)
-        L.append(K)
-        U.append(K)
-        U[0][i]=A[0][i]
-
-    for j in range(0,n):
-        for i in range(1,n):
-            if i<j or i==j:
-                s=0
-                for k in range(1,i):
-                    s = s  + (float(L[i][k]))*(float(U[k][j]))
-                    print(s)
-                L[i][j]= A[i][j] - s
-            else:
-                pass
-    for j in range(0,n):
-        for i in range(1,n):
-            if i>j:
-                s=0
-                for k in range(1,j):
-                    s = s + (L[i][k])*(U[k][j])
-                L[i][j]= (A[i][j] - s)/(U[j][j])
-            else:
-                pass
-        
-    return L,U
-
-        
-
-
-        
     
+    # Doolittle LU Decomposition Algorithm
+    for i in range(n):
+        # Calculate U matrix elements (upper triangular)
+        for k in range(i, n):
+            sum_val = 0
+            for j in range(i):
+                sum_val += A[i][j] * A[j][k]
+            A[i][k] = A[i][k] - sum_val
+        
+        # Calculate L matrix elements (lower triangular)
+        for k in range(i+1, n):
+            sum_val = 0
+            for j in range(i):
+                sum_val += A[k][j] * A[j][i]
+            A[k][i] = (A[k][i] - sum_val) / A[i][i]
+    
+    return A
+
+def solve_linear_equations(A, b):
+    """
+    This fuction solves system of linear equations using LU decomposition
+    with forward-backward substitution.
+   
+    """
+    n = len(A)
+    
+    # Step 1: Get LU decomposition
+    LU_combined = LU_decomposition(A)
+    
+    # Step 2: Extract L and U matrices from LU
+    L = []
+    U = []
+    for i in range(n):
+        L_row = []
+        U_row = []
+        for j in range(n):
+            if i == j:
+                L_row.append(1.0)  # Diagonal of L is 1 
+                U_row.append(LU_combined[i][j])
+            elif i > j:
+                L_row.append(LU_combined[i][j])  # Lower triangular
+                U_row.append(0.0)
+            else:
+                L_row.append(0.0)
+                U_row.append(LU_combined[i][j])  # Upper triangular
+        L.append(L_row)
+        U.append(U_row)
+    
+    # Step 3: Forward Substitution - Solve Ly = b
+    y = []
+    for i in range(n):
+        sum_val = 0
+        for j in range(i):
+            sum_val += L[i][j] * y[j]
+        y.append(b[i] - sum_val)
+    
+    # Step 4: Backward Substitution - Solve Ux = y
+    x = [0.0] * n
+    for i in range(n-1, -1, -1):
+        sum_val = 0
+        for j in range(i+1, n):
+            sum_val += U[i][j] * x[j]
+        x[i] = (y[i] - sum_val) / U[i][i]
+    
+    return x
